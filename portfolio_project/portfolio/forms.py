7e6068b796307f55bf36630c138df4dd9b0df824
neexal <nishanghimire111@gmail.com>
2024-08-07 22:08:01 +0545

    Copied

--- portfolio_project/portfolio/forms.py
+++ portfolio_project/portfolio/forms.py
@@ -1,4 +1,18 @@
-# portfolio/forms.py
+# # portfolio/forms.py
+
+# from django import forms
+# from .models import Contact
+
+# class ContactForm(forms.ModelForm):
+#     class Meta:
+#         model = Contact
+#         fields = ['name', 'email', 'about_project']
+#         widgets = {
+#             'name': forms.TextInput(attrs={'placeholder': 'Your Name'}),
+#             'email': forms.EmailInput(attrs={'placeholder': 'Your Email'}),
+#             'about_project': forms.Textarea(attrs={'placeholder': 'About Project'}),
+#         }
+
 
 from django import forms
 from .models import Contact
@@ -8,7 +22,7 @@ class ContactForm(forms.ModelForm):
         model = Contact
         fields = ['name', 'email', 'about_project']
         widgets = {
-            'name': forms.TextInput(attrs={'placeholder': 'Your Name'}),
-            'email': forms.EmailInput(attrs={'placeholder': 'Your Email'}),
-            'about_project': forms.Textarea(attrs={'placeholder': 'About Project'}),
+            'name': forms.TextInput(attrs={'placeholder': ' ', 'class': 'form-control name-input'}),
+            'email': forms.EmailInput(attrs={'placeholder': ' ', 'class': 'form-control email-input'}),
+            'about_project': forms.Textarea(attrs={'placeholder': ' ', 'class': 'form-control project-textarea'}),
         }
